a6ec08a2c51e193a9101c30de232c5f439601e6c
ddrraaggaarriiss <okay969696@gmail.com>
2023-04-18 00:11:12 +0300

    hw3

--- book/admin.py
+++ book/admin.py
@@ -4,5 +4,3 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Book)
-
-
--- phone_app/urls.py
+++ phone_app/urls.py
@@ -3,6 +3,6 @@ from phone_app.views import phone_all_view, phone_detail_view
 
 urlpatterns = [
     path('phone_list/', phone_all_view, name='phone_list'),
-    path('phone_list/<int:id>', phone_detail_view, name='phone_detail'),
+    path('phone_list/<int:id>/', phone_detail_view, name='phone_detail'),
 ]
 
