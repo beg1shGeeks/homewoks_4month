d59ef70c8249c67d62a7745ebb562fab491ea723
ddrraaggaarriiss <okay969696@gmail.com>
2023-04-25 17:08:15 +0300

    test

--- phone_app/admin.py
+++ phone_app/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from phone_app.models import Phones
+from phone_app.models import Phones, Reviews
 
 # Register your models here.
 admin.site.register(Phones)
+admin.site.register(Reviews)
+
+
